ebb8cb59fe41f98fa2d5280b0c5b2dc9f99d7f10
beto-machado <luisrobertobeto3@gmail.com>
2024-11-14 22:16:25 -0300

    change app.js

--- src/App.js
+++ src/App.js
@@ -5,14 +5,14 @@ import Weather from "./components/Weather";
 
 function App() {
   return (
-    <div className="App bg-gradient-to-r from-blue-400 via-blue-500 to-blue-600 min-h-screen text-white">
+    <div className="App flex flex-col min-h-screen bg-gradient-to-r from-blue-400 via-blue-500 to-blue-600 text-white">
       <header className="flex justify-center items-center py-10">
         <h1 className="text-5xl font-extrabold tracking-wide text-center">
           Consulta de Clima
         </h1>
       </header>
 
-      <main className="flex justify-center items-center py-8">
+      <main className="flex-grow flex justify-center items-center py-8">
         <div className="w-full max-w-xl px-6 py-8 bg-white rounded-lg shadow-lg">
           <Weather />
         </div>
@@ -20,9 +20,9 @@ function App() {
 
       <footer className="text-center py-4 bg-gray-800">
         <p className="text-sm">
-          Desenvolvido por <strong>Seu Nome</strong> -{" "}
+          Desenvolvido por <strong>Beto Machado</strong> -{" "}
           <a
-            href="https://www.linkedin.com/in/beto-machado"
+            href="https://www.linkedin.com/in/betomachado3"
             target="_blank"
             rel="noopener noreferrer"
             className="text-blue-400 hover:text-blue-600 transition-colors duration-200"
@@ -36,3 +36,4 @@ function App() {
 }
 
 export default App;
+
